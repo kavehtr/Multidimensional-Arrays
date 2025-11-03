f35a0edf5a3b2c27ec3cc81abeed7629e6846012
kavehtr <kavehtr@email.com>
2025-11-03 22:13:22 +0330

    init

---
+++ main.ts
@@ -0,0 +1,13 @@
+function transformMatrix(matrix: number[][]): number[][] {
+  const rows: number = matrix.length;
+  const cols: number = rows > 0 ? matrix[0].length : 0;
+  const result: number[][] = [];
+
+  return result;
+}
+
+let matrix: number[][] = [
+  [101, 102, 103, 104],
+  [201, 202, 203, 204],
+  [301, 302, 303, 304],
+];
